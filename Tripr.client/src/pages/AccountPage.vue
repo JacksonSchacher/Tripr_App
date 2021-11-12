4795a1add8ac9e8cf1c4b1f8def677d59e9d2b74
Scott Fennie <s.fennie7@gmail.com>
2021-11-11 18:14:25 -0700

    fixed last trip not loading

--- Tripr.client/src/pages/AccountPage.vue
+++ Tripr.client/src/pages/AccountPage.vue
@@ -6,44 +6,59 @@
           <h4 class="mt-2 font-m" v-if="account">
             {{ account.name }}
           </h4>
-          <img v-if="account.name === 'Scooter' || 'jackson@tripr.com' || 'Nathan'" class="pin mt-3" src="../assets/img/pin.png" alt="nope">
+          <img
+            v-if="account.name === 'Scooter' || 'jackson@tripr.com' || 'Nathan'"
+            class="pin mt-3"
+            src="../assets/img/pin.png"
+            alt="nope"
+          />
         </div>
-        <p>last road trip: <span>{{ }}</span></p>
-        <p>total trips: <span>{{ mytrips.length }}</span></p>
-        <img v-if="account"
-             :src="account.picture"
-             alt="account picture"
-             class="accountPicture rounded-circle"
-             data-bs-toggle="modal"
-             data-bs-target="#editprofile-modal"
-        >
+        <p v-if="lasttrip">
+          last road trip: <span>{{ lasttrip.trip.title }}</span>
+        </p>
+        <p>
+          total trips: <span>{{ mytrips.length }}</span>
+        </p>
+        <img
+          v-if="account"
+          :src="account.picture"
+          alt="account picture"
+          class="accountPicture rounded-circle"
+          data-bs-toggle="modal"
+          data-bs-target="#editprofile-modal"
+        />
       </div>
     </div>
     <div class="row justify-content-center m-0 p-0">
       <button type="button" class="m-0 p-0 btn w-100">
         <router-link :to="{ name: 'CreateTrip' }" class="">
           <h2 class="m-0 p-0">
-            <img class=" cool-button patch mb-5" src="../assets/img/create.png" alt="Create Trip">
+            <img
+              class="cool-button patch mb-5"
+              src="../assets/img/create.png"
+              alt="Create Trip"
+            />
           </h2>
         </router-link>
-        <img class="cool-button patch m-0 mb-4" src="../assets/img/join.png" alt="Join Trip" data-bs-toggle="modal" data-bs-target="#join-modal">
+        <img
+          class="cool-button patch m-0 mb-4"
+          src="../assets/img/join.png"
+          alt="Join Trip"
+          data-bs-toggle="modal"
+          data-bs-target="#join-modal"
+        />
       </button>
     </div>
     <div class="mt-1 row align-items-end justify-content-between">
       <div class="col-6">
         <button type="button" class="ms-2 bg-primary btn w-100">
           <router-link :to="{ name: 'YourTrips' }" class="">
-            <h2 class="text-shadow">
-              Trips
-            </h2>
+            <h2 class="text-shadow">Trips</h2>
           </router-link>
         </button>
       </div>
       <div class="d-flex col-6 pe-4 justify-content-end">
-        <div
-          class="text-primary"
-          @click="logout"
-        >
+        <div class="text-primary" @click="logout">
           <i class="mdi mdi-logout-variant f-30"></i>
         </div>
       </div>
@@ -59,9 +74,7 @@
   </Modal>
   <Modal id="join-modal">
     <template #modal-title>
-      <h5 class="join-modal-text text-center">
-        Join Trip
-      </h5>
+      <h5 class="join-modal-text text-center">Join Trip</h5>
     </template>
     <template #modal-body>
       <JoinTripForm />
@@ -77,7 +90,7 @@ import { tripsService } from '../services/TripsService'
 export default {
   name: 'Account',
   setup() {
-    onMounted(async() => {
+    onMounted(async () => {
       await tripsService.getAllTrips()
       await tripsService.getAllMyTrackedTrips()
     })
@@ -96,22 +109,22 @@ export default {
 <style scoped lang="scss">
 .btn {
   border-radius: 10px;
-  padding: .1rem;
+  padding: 0.1rem;
 }
 h2 {
-  font-family: museo-slab,serif;
+  font-family: museo-slab, serif;
   font-size: 2rem;
   font-weight: 600;
   margin-bottom: 0;
 }
-.font-m{
-  font-family: museo-slab,serif;
+.font-m {
+  font-family: museo-slab, serif;
   font-weight: 600;
 }
 .account {
   height: 100vh;
 }
-.account-info-wrapper{
+.account-info-wrapper {
   filter: drop-shadow(0px 5px 5px rgba(20, 19, 20, 0.25));
 }
 .account-info {
@@ -120,48 +133,47 @@ h2 {
   width: 90vw;
   height: 35vh;
   margin-bottom: -60px;
-//  -moz-box-shadow:    inset 0 0 10px #000000;
-//    -webkit-box-shadow: inset 0 0 10px #000000;
-//    box-shadow:         inset 0 0 10px #000000;
+  //  -moz-box-shadow:    inset 0 0 10px #000000;
+  //    -webkit-box-shadow: inset 0 0 10px #000000;
+  //    box-shadow:         inset 0 0 10px #000000;
 }
 .patch {
   width: 55vw;
   height: auto;
-  margin:-50px ;
+  margin: -50px;
 }
 .text-shadow {
   border-radius: 10px;
-  padding: .5rem 1rem;
-  border: 3px dashed rgba(255,255,255,0.5);
-  color: #A28558;
-  text-shadow: 0px 2px 2px rgba(255,255,255,0.5);
+  padding: 0.5rem 1rem;
+  border: 3px dashed rgba(255, 255, 255, 0.5);
+  color: #a28558;
+  text-shadow: 0px 2px 2px rgba(255, 255, 255, 0.5);
 }
-.cool-button{
+.cool-button {
+}
+.cool-button:hover {
+  cursor: pointer;
+  transform: scale(1.03);
+}
 
-}
-.cool-button:hover{
-cursor: pointer;
- transform: scale(1.03);
-}
-
-.accountPicture{
+.accountPicture {
   height: 48px;
   width: 48px;
   object-fit: cover;
 }
-.inset{
-     -moz-box-shadow:    inset 0 0 10px #000000;
-   -webkit-box-shadow: inset 0 0 10px #000000;
-   box-shadow:         inset 0 0 10px #000000;
+.inset {
+  -moz-box-shadow: inset 0 0 10px #000000;
+  -webkit-box-shadow: inset 0 0 10px #000000;
+  box-shadow: inset 0 0 10px #000000;
 }
-.pin{
-height: 32px;
-width: 42px;
+.pin {
+  height: 32px;
+  width: 42px;
 }
 
-.join-modal-text{
-  color:#A28558;
-  font-family: museo-slab,serif;
+.join-modal-text {
+  color: #a28558;
+  font-family: museo-slab, serif;
   font-size: 2rem;
   font-weight: 600;
   margin-bottom: 0;
