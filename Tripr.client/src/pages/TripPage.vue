dc37fc572ff42a12390e5031c5959b356a6c00f2
JacksonSchacher <84756850+JacksonSchacher@users.noreply.github.com>
2021-11-15 13:05:19 -0800

    commented out number of locations

--- Tripr.client/src/pages/TripPage.vue
+++ Tripr.client/src/pages/TripPage.vue
@@ -17,9 +17,9 @@
               Route Details
             </h4>
           </div>
-          <p v-if="trip.geo.features" class="d-flex justify-content-end pe-2">
+          <!-- <p v-if="trip.geo.features" class="d-flex justify-content-end pe-2">
             Locations: {{ trip.geo.features.length }}
-          </p>
+          </p> -->
         </div>
       </div>
       <div>
